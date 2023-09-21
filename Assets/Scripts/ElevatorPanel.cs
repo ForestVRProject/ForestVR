9c8596e03ac6afd9db5ef918653351b10220b55f
lsjsos <lsjsos@konkuk.ac.kr>
2023-09-21 22:42:53 +0900

    Map dev#1

---
+++ Assets/Scripts/ElevatorPanel.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorPanel : MonoBehaviour
+{
+    private ElevatorTrigger _elevator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _elevator = GameObject.Find("elevator1f").GetComponent<ElevatorTrigger>();
+
+        if (_elevator == null)
+        {
+            Debug.LogError("Elevator is Null");
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            _elevator.CallElevator();
+        }
+    }
+
+}
---
+++ Assets/Scripts/ElevatorTrigger.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform _origin, _target;
+    [SerializeField] private float _speed;
+    public Animation leftDoor2f;
+
+    private bool _goingUp = false;
+
+    private void Start()
+    {
+        leftDoor2f = GameObject.Find("left_door2f").GetComponent<Animation>();
+    }
+
+    public void CallElevator()
+    {
+        _goingUp = !_goingUp;
+    }
+
+    private void FixedUpdate()
+    {
+        if(_goingUp == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _origin.position, _speed * Time.deltaTime);
+            leftDoor2f.Play("OpenLeftDoor");
+
+        }
+        else if(_goingUp == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _target.position, _speed * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _goingUp = !_goingUp;
+            other.transform.parent = this.transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.transform.parent = null;
+        }
+    }
+    
+}
