78f140b121fb8c703f7ec67f32e92f8ef427a92e
heonyBoogie <98378185+heonyBoogie@users.noreply.github.com>
2023-10-06 02:57:29 +0900

    add elevator sound
    animation start key
    7 : shanking hand -> sitting
    8 : sitting -> hugging
    9 : hugging -> shanking hand

--- Assets/Scripts/ElevatorMoving.cs
+++ Assets/Scripts/ElevatorMoving.cs
@@ -11,6 +11,7 @@ public class Elevator : MonoBehaviour
     [SerializeField] private Transform _origin, _target; // elevator waypoints
     [SerializeField] private float _speed = 3.0f;
     [SerializeField] private GameObject _player;
+    private AudioSource _audioSource;
     public Animation leftDoor;
     public Animation rightDoor;
 
@@ -21,6 +22,7 @@ public class Elevator : MonoBehaviour
     private void Start()
     {
         Debug.Log("ElevatorMoving script is attached");
+        _audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -33,13 +35,14 @@ public class Elevator : MonoBehaviour
             float distance = direction.magnitude;
 
             if (distance > 0.1f)
-            {
+            {              
                 Vector3 moveAmount = direction.normalized * _speed * Time.fixedDeltaTime;
                 transform.position = Vector3.MoveTowards(transform.position, _target.position, moveAmount.magnitude);
             }
             else
             {
                 _goingDown = false;
+                _audioSource.Stop();
                 OpenDoor();
             }
         }else if (_goingUp)
@@ -56,6 +59,7 @@ public class Elevator : MonoBehaviour
             else
             {
                 _goingUp = false;
+                _audioSource.Stop();
                 OpenDoor();
             }
         }        
@@ -63,7 +67,7 @@ public class Elevator : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {    
-        if(other.tag == "Player" )
+        if(other.tag == "Player")
         {
             Debug.Log("enter elevator");
             CloseDoor();
@@ -113,11 +117,13 @@ public class Elevator : MonoBehaviour
         {
             Debug.Log("Going Down");
             _goingDown = true;
+            _audioSource.Play();
         }
         else
         {
             Debug.Log("Going Up");
             _goingUp = true;
+            _audioSource.Play();
         }
     }
 }
