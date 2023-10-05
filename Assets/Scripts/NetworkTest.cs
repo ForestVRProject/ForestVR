01275f90e67333fbf641f17ab2bdfbccbf54e397
heonyBoogie <98378185+heonyBoogie@users.noreply.github.com>
2023-10-06 01:23:07 +0900

    add shanking animation

--- Assets/Scripts/NetworkTest.cs
+++ Assets/Scripts/NetworkTest.cs
@@ -5,28 +5,39 @@ using UnityEngine.Networking;
 
 public class NetworkTest : MonoBehaviour
 {
-    int sampleRate = 44100;
-    public float[] samples;
-    public float rmsValue;
-    public float modulate;
-    public int resultValue;
-    public int cutValue;
     public string conversation_id = "1fa05011-697a-4c7d-9daa-01d9bc0a7047";
-    public string message = "jerry��, ���� �������� � ���� ��ó�� [����]���� ���� ��ó�ΰ���?";
-    public AudioClip aud;
+    public string message = "";
     public Animator anim; //hug anim test
     // Start is called before the first frame update
     void Start()
     {
-        samples = new float[sampleRate];
-        aud = GetComponent<AudioSource>();
         //StartCoroutine(UnityWebRequestStartConversationPOST());
         //StartCoroutine(UnityWebRequestContinueConversationPOST(conversation_id, message));
-        StartCoroutine(UnityWebRequestKoreanTextRecognitionPOST());
+        //StartCoroutine(UnityWebRequestKoreanTextRecognitionPOST());
         //StartCoroutine(UnityWebRequestGet());
         anim = GetComponent<Animator>();//hug anim test
     }
 
+    
+    private void Update()
+    {
+        /*hug anim test*/
+        if(Input.GetKeyDown(KeyCode.Alpha8)) 
+        {
+            anim.SetTrigger("Hug");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            anim.SetTrigger("ShankingHand");
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            anim.SetTrigger("Sit");
+        }
+        /*hug anim test*/
+    }
     IEnumerator UnityWebRequestKoreanTextSynthesisPOST()
     {
         string url = "";
@@ -90,7 +101,7 @@ public class NetworkTest : MonoBehaviour
         WWWForm form = new WWWForm();
         form.AddBinaryData("file", null, "Assets/2.mp3");
         UnityWebRequest www = UnityWebRequest.Post(url, form);  // ���� �ּҿ� ������ �Է�
-        
+
         yield return www.SendWebRequest();  // ���� ���
 
         if (www.error == null)
@@ -102,24 +113,5 @@ public class NetworkTest : MonoBehaviour
             Debug.Log("error");
         }
     }
-
-    public void UserVoice()
-    {
-        aud.clip = Microphone.Start(Microphone.devices[0].ToString(), false, 5, sampleRate); //���� 5��
-        EncodeMP3.convert(aud.clip, Application.dataPath + $"UserVoice{voiceNum}", 128); //������ clip�� mp3 ���Ϸ� ��ȯ�ؼ� �ش� ��ο� ����
-        voiceNum += 1;
-        /*aud.GetData(samples, 0);
-        float sum = 0;
-        for(int i = 0; i < samples.Length; i++)
-        {
-            sum += samples[i] * samples[i];
-        }
-        rmsValue = Mathf.Sqrt(sum / samples.Length);
-        rmsValue = rmsValue * modulate;
-        rmsValue = Mathf.Clamp(rmsValue, 0, 100);
-        resultValue = Mathf.RoundToInt(rmsValue);
-        if (resultValue < cutValue)
-            resultValue = 0;
-        */
-    }
 }
+
