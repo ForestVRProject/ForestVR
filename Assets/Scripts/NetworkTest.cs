63b2e75d1e6fe6fd462ac8cec898f4d12cce1afd
Jerry <50892930+lsjsos@users.noreply.github.com>
2023-10-05 19:08:29 +0900

    Network Test#4

--- Assets/Scripts/NetworkTest.cs
+++ Assets/Scripts/NetworkTest.cs
@@ -13,28 +13,23 @@ public class NetworkTest : MonoBehaviour
     public int cutValue;
     public string conversation_id = "1fa05011-697a-4c7d-9daa-01d9bc0a7047";
     public string message = "jerry님, 지금 떠오르는 어린 시절 상처는 [누구]에게 받은 상처인가요?";
-    public AudioClip aud;
+    public AudioSource aud;
+    public int voiceNum = 1;
     // Start is called before the first frame update
     void Start()
     {
         samples = new float[sampleRate];
+        aud = GetComponent<AudioSource>();
         //StartCoroutine(UnityWebRequestStartConversationPOST());
         //StartCoroutine(UnityWebRequestContinueConversationPOST(conversation_id, message));
         StartCoroutine(UnityWebRequestKoreanTextRecognitionPOST());
-        //StartCoroutine(UnityWebRequestGet());
     }
 
-    private void Update()
+    IEnumerator UnityWebRequestKoreanTextSynthesisPOST()
     {
-        //UserVoice();
-    }
-
-    IEnumerator UnityWebRequestGet()
-    {
-        string apikey = "";
-
-        string url = $"https://api.neople.co.kr/df/skills/jobId?jobGrowId=jobGrowId&apikey={apikey}";
-        UnityWebRequest www = UnityWebRequest.Get(url); //요청을 보냄
+        string url = "";
+        WWWForm form = new WWWForm();
+        UnityWebRequest www = UnityWebRequest.Post(url, form); //요청을 보냄
 
         yield return www.SendWebRequest();
 
@@ -53,11 +48,7 @@ public class NetworkTest : MonoBehaviour
         string url = "http://sd-church.duckdns.org:8000/start_conversation";
         WWWForm form = new WWWForm();
         string username = "jerry";
-        string test_data = "jerry is testing post";
-        //string pw = "비밀번호";
         form.AddField("username", username);
-        form.AddField("Data", test_data);
-        //form.AddField("Password", pw);
         UnityWebRequest www = UnityWebRequest.Post(url, form);  // 보낼 주소와 데이터 입력
 
         yield return www.SendWebRequest();  // 응답 대기
@@ -73,9 +64,8 @@ public class NetworkTest : MonoBehaviour
     }
     IEnumerator UnityWebRequestContinueConversationPOST(string conversation_id, string message)
     {
-        string url = "http://sd-church.duckdns.org:8000/continue_conversation/1fa05011-697a-4c7d-9daa-01d9bc0a7047?message=jerry%EB%8B%98%2C%20%EC%A7%80%EA%B8%88%20%EB%96%A0%EC%98%A4%EB%A5%B4%EB%8A%94%20%EC%96%B4%EB%A6%B0%20%EC%8B%9C%EC%A0%88%20%EC%83%81%EC%B2%98%EB%8A%94%20%5B%EB%88%84%EA%B5%AC%5D%EC%97%90%EA%B2%8C%20%EB%B0%9B%EC%9D%80%20%EC%83%81%EC%B2%98%EC%9D%B8%EA%B0%80%EC%9A%94%3F";
+        string url = "http://sd-church.duckdns.org:8000/continue_conversation}";
         WWWForm form = new WWWForm();
-        //string pw = "비밀번호";
         form.AddField("conversation_id", conversation_id);
         form.AddField("message", message);
         //form.AddField("Password", pw);
@@ -96,11 +86,9 @@ public class NetworkTest : MonoBehaviour
     {
         string url = "http://sd-church.duckdns.org:8000/korean-text-recognition";
         WWWForm form = new WWWForm();
-        //string pw = "비밀번호";
-        form.AddField("file", "Assets/2.mp3");
-        //form.AddField("Password", pw);
+        form.AddBinaryData("file", null, "Assets/2.mp3");
         UnityWebRequest www = UnityWebRequest.Post(url, form);  // 보낼 주소와 데이터 입력
-
+        
         yield return www.SendWebRequest();  // 응답 대기
 
         if (www.error == null)
@@ -115,8 +103,10 @@ public class NetworkTest : MonoBehaviour
 
     public void UserVoice()
     {
-        AudioClip aud = Microphone.Start(Microphone.devices[0].ToString(), true, 1, sampleRate);
-        aud.GetData(samples, 0);
+        aud.clip = Microphone.Start(Microphone.devices[0].ToString(), false, 5, sampleRate); //녹음 5초
+        EncodeMP3.convert(aud.clip, Application.dataPath + $"UserVoice{voiceNum}", 128); //녹음된 clip을 mp3 파일로 변환해서 해당 경로에 저장
+        voiceNum += 1;
+        /*aud.GetData(samples, 0);
         float sum = 0;
         for(int i = 0; i < samples.Length; i++)
         {
@@ -128,5 +118,6 @@ public class NetworkTest : MonoBehaviour
         resultValue = Mathf.RoundToInt(rmsValue);
         if (resultValue < cutValue)
             resultValue = 0;
+        */
     }
 }
