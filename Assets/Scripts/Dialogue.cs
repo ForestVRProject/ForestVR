7672343406ca6c6fe6e04896e122a2d88c191d20
Jerry <50892930+lsjsos@users.noreply.github.com>
2024-01-03 22:01:28 +0900

    Update All

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -9,7 +9,11 @@ public class Dialogue : MonoBehaviour
     [SerializeField]
     private string[] textdata;
     public int i = 0;
+    public bool isSelected = false;
     public TextMeshProUGUI textShow;
+    public GameObject btn1;
+    public GameObject btn2;
+    public GameObject btn3;
 
     private void Start()
     {
@@ -28,8 +32,52 @@ public class Dialogue : MonoBehaviour
             Guide.instance.GuideSwitch();
             textShow.SetText(textdata[i]);
             yield return new WaitWhile(()=> Guide.instance.guideAud.isPlaying == true);
+            if (i == 4)
+            {
+                SelectDialogue();
+                yield return new WaitWhile(() => isSelected == false);
+                continue;
+            }
             i++;
             yield return new WaitForSeconds(1);
         }
+    }
+
+    public void SelectDialogue()
+    {
+        btn1.SetActive(true);
+        btn2.SetActive(true);
+        btn3.SetActive(true);
+    }
+
+    public void SelectBtn1()
+    {
+        //특정 번호의 대사로 이동
+        i = 6;
+        Guide.instance.i = 6;
+        btn1.SetActive(false);
+        btn2.SetActive(false);
+        btn3.SetActive(false);
+        isSelected = true;
+    }
+
+    public void SelectBtn2()
+    {
+        i = 8;
+        Guide.instance.i = 8;
+        btn1.SetActive(false);
+        btn2.SetActive(false);
+        btn3.SetActive(false);
+        isSelected = true;
+    }
+
+    public void SelectBtn3()
+    {
+        i = 7;
+        Guide.instance.i = 7;
+        btn1.SetActive(false);
+        btn2.SetActive(false);
+        btn3.SetActive(false);
+        isSelected = true;
     }
 }
--- Assets/Scripts/SunControl.cs
+++ Assets/Scripts/SunControl.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 public class SunControl : MonoBehaviour
 {
     public Material defaultSkyMat;
-    public Material sunsetSkyMat;
-    public Material eveningSkyMat;
+    //public Material eveningSkyMat;
+    public Light directionalLight;
     // Start is called before the first frame update
     void Start()
     {
         RenderSettings.skybox = defaultSkyMat;
+        RenderSettings.skybox.SetColor("_Tint", new Color32(128, 128, 128, 255));
         StartCoroutine(SunMove());
     }
 
     IEnumerator SunMove()
     {
-        yield return new WaitForSeconds(9);
-        RenderSettings.skybox = sunsetSkyMat;
-        yield return new WaitForSeconds(9);
-        RenderSettings.skybox = eveningSkyMat;
+        yield return new WaitForSeconds(3);
+        RenderSettings.skybox.SetColor("_Tint", new Color32(128, 128, 90, 255));
+        yield return new WaitForSeconds(3);
+        RenderSettings.skybox.SetColor("_Tint", new Color32(255, 128, 60, 255));
+        directionalLight.color = new Color32(201, 109, 127, 255);
+        yield return new WaitForSeconds(3);
+        RenderSettings.skybox.SetColor("_Tint", new Color32(100, 50, 30, 255));
+        yield return new WaitForSeconds(3);
+        //RenderSettings.skybox = eveningSkyMat;
+        directionalLight.color = new Color32(87, 68, 111, 255);
+    }
+
+    private void FixedUpdate()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * -1.5f);
     }
 }
